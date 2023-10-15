e1e60981bb0fa4589efbd8b196aa43fa9435efe9
tzx <tzx@tzxdeMacBook-Pro.local>
2023-10-15 11:14:59 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// 
+</script>
+
+<template>
+  <view class="index">
+    <uni-card title="基础卡片">
+
+    </uni-card>
+  </view>
+</template>
+
+<style lang="scss">
+//
+</style>
---
+++ src/pages/my/my.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { useMemberStore } from '@/stores'
+import { http } from '@/utils/http'
+
+const memberStore = useMemberStore()
+const testInternet = async () => {
+  const res = await http({
+    url: '/home/banneraa',
+    method: 'GET',
+    header: {},
+  })
+  console.log('jieguo: ', res)
+}
+</script>
+
+<template>
+  <view class="my">
+    <view>会员信息：{{ memberStore.profile }}</view>
+    <button @tap="
+      memberStore.setProfile({
+        nickname: '黑马先锋',
+      })
+      " size="mini" plain type="primary">
+      保存用户信息
+    </button>
+    <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button @click="testInternet">测试网络</button>
+  </view>
+</template>
+
+<style lang="scss">
+//
+</style>
